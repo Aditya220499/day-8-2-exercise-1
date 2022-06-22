f29e6746fdde032526d0488397c974990c21ed22
Aditya220499 <64396269+Aditya220499@users.noreply.github.com>
2022-06-22 16:12:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+#Write your code below this line 👇
+def prime_checker(number):
+  if number == 1:
+    print("It's nither prime nor composite number.")
+    return
+  for i in range(2,number//2 + 1):
+    if number % i == 0:
+      print('It\'s not a prime number.')
+      return
+      break
+  
+  print("It's a prime number.")
+
+
+
+
+#Write your code above this line 👆
+    
+#Do NOT change any of the code below👇
+n = int(input("Check this number: "))
+prime_checker(number=n)
+
+
+
